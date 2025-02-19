d122aac1c7298a1f7e5534d3edb696aeeade2d0b
LaiXinpeng <161671631+LaiXinpeng@users.noreply.github.com>
2025-02-19 19:32:46 +0800

    backup

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Git 操作
+git add .
+git commit -m "backup"
+git push origin master
+
+# Hexo 生成和部署
+hexo g
+hexo d
+
+# 完成提示
+echo "Finish"
+
+# 等待用户输入，兼容 Linux、macOS 和 Windows
+if [ "$(uname)" == "Darwin" ] || [ "$(uname)" == "Linux" ]; then
+  # Linux 和 macOS 使用 read 命令
+  echo "按任意键继续"
+  read -n 1
+else
+  # Windows 使用 pause 命令
+  echo "按任意键继续"
+  pause
+fi
