fd62fb68ed0f19435ea18f1bf7fcf10c76a28da6
LaiXinpeng <161671631+LaiXinpeng@users.noreply.github.com>
2025-02-19 19:34:35 +0800

    backup

--- deploy.bat
+++ deploy.bat
@@ -1,22 +1,17 @@
-# Git 操作
+@echo off
+
+rem Git 操作
 git add .
 git commit -m "backup"
 git push origin master
 
-# Hexo 生成和部署
+rem Hexo 生成和部署
 hexo g
 hexo d
 
-# 完成提示
-echo "Finish"
+rem 完成提示
+echo Finish
 
-# 等待用户输入，兼容 Linux、macOS 和 Windows
-if [ "$(uname)" == "Darwin" ] || [ "$(uname)" == "Linux" ]; then
-  # Linux 和 macOS 使用 read 命令
-  echo "按任意键继续"
-  read -n 1
-else
-  # Windows 使用 pause 命令
-  echo "按任意键继续"
-  pause
-fi
+rem 等待用户按任意键继续
+echo 按任意键继续
+pause >nul
